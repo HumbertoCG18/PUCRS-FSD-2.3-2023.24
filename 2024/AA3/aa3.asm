61d5596b727a7a906d4ac67b7ac314407e3618d9
Humberto Corrêa Gomes <humberto.cgomes17@gmail.com>
2024-07-09 15:28:19 -0300

    Update aa3.asm

--- 2024/AA3/aa3.asm
+++ 2024/AA3/aa3.asm
@@ -1,102 +1,118 @@
 .data
-PE: .word 0       # Address in Data Segment: 0x10010000
-n: .word 8
-A: .word 1, 2, 3, 4, 5, 6, 7, -8
-B: .word -8, 7, -6, 5, 4, -3, 2, -1
-C: .space 32  # 8 words * 4 bytes per word
-D: .space 32  # 8 words * 4 bytes per word
+vetor_A: .word 10, -5, 8, -3, 15, 6, -7, 4
+vetor_B: .word -2, 7, -10, 12, 3, -6, 9, -1
+tamanho_vetor: .word 8   # Tamanho dos vetores A e B
 
 .text
 .globl main
 
 main:
-    # Inicializa PE com 0
-    la $t0, PE        # Carrega o endereço de PE em $t0
-    li $t1, 0         # Carrega o valor 0 em $t1
-    sw $t1, 0($t0)    # Armazena 0 em PE
+    # Inicialização dos registradores
+    li $t0, 0             # Índice para percorrer os vetores A e B
+    la $s0, vetor_A       # Endereço do vetor A
+    la $s1, vetor_B       # Endereço do vetor B
+    la $s2, vetor_C       # Endereço do vetor C (resultado da adição)
+    la $s3, vetor_D       # Endereço do vetor D (resultado da subtração)
+    lw $s4, tamanho_vetor # Tamanho dos vetores A e B
+    
+    # Loop para inicializar os vetores C e D com zero
+    init_loop:
+        beq $t0, $s4, end_init_loop  # Se índice igual ao tamanho, sai do loop
+        
+        # Inicializa vetor C[i] com zero
+        sw $zero, ($s2)
+        addi $s2, $s2, 4
+        
+        # Inicializa vetor D[i] com zero
+        sw $zero, ($s3)
+        addi $s3, $s3, 4
+        
+        addi $t0, $t0, 1  # Incrementa o índice
+        j init_loop      # Volta ao início do loop
+    
+    end_init_loop:
+    # Chamada das sub-rotinas de adição e subtração
+    jal adicao_vetores   # Chama a sub-rotina de adição
+    jal subtracao_vetores  # Chama a sub-rotina de subtração
+    
+    # Chamada da sub-rotina de produto escalar
+    jal produto_escalar
+    
+    # Finaliza o programa
+    li $v0, 10
+    syscall
 
-    # Carrega o número de elementos (n)
-    la $t1, n         # Carrega o endereço de n em $t1
-    lw $t1, 0($t1)    # Carrega o valor de n em $t1
+# Sub-rotina para adicionar vetores A e B, armazenando em C
+adicao_vetores:
+    # Reinicializa o índice
+    li $t0, 0
+    
+    add_loop:
+        beq $t0, $s4, end_add_loop  # Se índice igual ao tamanho, sai do loop
+        
+        lw $s5, ($s0)    # Carrega A[i] em $s5
+        lw $s6, ($s1)    # Carrega B[i] em $s6
+        
+        add $s7, $s5, $s6  # Soma A[i] + B[i]
+        sw $s7, ($s2)      # Armazena em C[i]
+        
+        addi $s0, $s0, 4   # Avança para o próximo elemento de A
+        addi $s1, $s1, 4   # Avança para o próximo elemento de B
+        addi $s2, $s2, 4   # Avança para o próximo elemento de C
+        
+        addi $t0, $t0, 1   # Incrementa o índice
+        j add_loop         # Volta ao início do loop
+    
+    end_add_loop:
+    jr $ra  # Retorna à chamada
 
-    # Carrega os endereços base dos vetores A e B
-    la $t2, A         # Carrega o endereço de A em $t2
-    la $t3, B         # Carrega o endereço de B em $t3
+# Sub-rotina para subtrair vetores A e B, armazenando em D
+subtracao_vetores:
+    # Reinicializa o índice
+    li $t0, 0
+    
+    sub_loop:
+        beq $t0, $s4, end_sub_loop  # Se índice igual ao tamanho, sai do loop
+        
+        lw $s5, ($s0)    # Carrega A[i] em $s5
+        lw $s6, ($s1)    # Carrega B[i] em $s6
+        
+        sub $s7, $s5, $s6  # Subtrai A[i] - B[i]
+        sw $s7, ($s3)      # Armazena em D[i]
+        
+        addi $s0, $s0, 4   # Avança para o próximo elemento de A
+        addi $s1, $s1, 4   # Avança para o próximo elemento de B
+        addi $s3, $s3, 4   # Avança para o próximo elemento de D
+        
+        addi $t0, $t0, 1   # Incrementa o índice
+        j sub_loop         # Volta ao início do loop
+    
+    end_sub_loop:
+    jr $ra  # Retorna à chamada
 
-    # Inicializa o contador do loop i com 0
-    li $t4, 0         # Carrega o valor 0 em $t4 (i = 0)
-
-    # Loop principal para a adição de vetores
-loop_add:
-    beq $t4, $t1, loop_sub # se i == n, pula para subtração
-
-    # Carrega elementos A[i] e B[i]
-    lw $t5, 0($t2)    # Carrega A[i] em $t5
-    lw $t6, 0($t3)    # Carrega B[i] em $t6
-
-    # Adiciona A[i] e B[i] e armazena em C[i]
-    add $t7, $t5, $t6 # t7 = A[i] + B[i]
-    la $t8, C         # Carrega o endereço de C em $t8
-    add $t9, $t8, $t4 # t9 = endereço de C[i]
-    sw $t7, 0($t9)    # Armazena A[i] + B[i] em C[i]
-
-    # Incrementa os ponteiros dos vetores e o contador do loop
-    addi $t2, $t2, 4  # Move para o próximo elemento em A
-    addi $t3, $t3, 4  # Move para o próximo elemento em B
-    addi $t4, $t4, 4  # i++
-
-    j loop_add        # Salta de volta para o início do loop
-
-# Loop para a subtração de vetores
-loop_sub:
-    beq $t4, $t1, calc_dot_product # se i == n, pula para produto escalar
-
-    # Carrega elementos A[i] e B[i]
-    lw $t5, 0($t2)    # Carrega A[i] em $t5
-    lw $t6, 0($t3)    # Carrega B[i] em $t6
-
-    # Subtrai B[i] de A[i] e armazena em D[i]
-    sub $t7, $t5, $t6 # t7 = A[i] - B[i]
-    la $t8, D         # Carrega o endereço de D em $t8
-    add $t9, $t8, $t4 # t9 = endereço de D[i]
-    sw $t7, 0($t9)    # Armazena A[i] - B[i] em D[i]
-
-    # Incrementa os ponteiros dos vetores e o contador do loop
-    addi $t2, $t2, 4  # Move para o próximo elemento em A
-    addi $t3, $t3, 4  # Move para o próximo elemento em B
-    addi $t4, $t4, 4  # i++
-
-    j loop_sub        # Salta de volta para o início do loop
-
-# Produto escalar
-calc_dot_product:
-    li $t4, 0         # Reinicia o contador do loop para 0
-    li $t8, 0         # Inicializa a soma do produto escalar com 0
-
-loop_dot_product:
-    beq $t4, $t1, store_pe # se i == n, pula para armazenar PE
-
-    # Carrega elementos C[i] e D[i]
-    la $t2, C         # Carrega o endereço de C em $t2
-    la $t3, D         # Carrega o endereço de D em $t3
-    lw $t5, 0($t2)    # Carrega C[i] em $t5
-    lw $t6, 0($t3)    # Carrega D[i] em $t6
-
-    # Multiplica C[i] e D[i] e acumula em t8
-    mul $t7, $t5, $t6 # t7 = C[i] * D[i]
-    add $t8, $t8, $t7 # t8 += C[i] * D[i]
-
-    # Incrementa os ponteiros dos vetores e o contador do loop
-    addi $t2, $t2, 4  # Move para o próximo elemento em C
-    addi $t3, $t3, 4  # Move para o próximo elemento em D
-    addi $t4, $t4, 4  # i++
-
-    j loop_dot_product# Salta de volta para o início do loop
-
-store_pe:
-    la $t0, PE        # Carrega o endereço de PE em $t0
-    sw $t8, 0($t0)    # Armazena o valor de PE na memória
-
-end:
-    # Fim do programa
-    j end             # Loop infinito para encerrar o programa
+# Sub-rotina para calcular o produto escalar entre vetores C e D
+produto_escalar:
+    # Reinicializa o índice e acumulador
+    li $t0, 0
+    li $t1, 0
+    
+    scalar_loop:
+        beq $t0, $s4, end_scalar_loop  # Se índice igual ao tamanho, sai do loop
+        
+        lw $s5, ($s2)    # Carrega C[i] em $s5
+        lw $s6, ($s3)    # Carrega D[i] em $s6
+        
+        mul $s7, $s5, $s6  # Calcula C[i] * D[i]
+        add $t1, $t1, $s7  # Acumula o resultado
+        
+        addi $s2, $s2, 4   # Avança para o próximo elemento de C
+        addi $s3, $s3, 4   # Avança para o próximo elemento de D
+        
+        addi $t0, $t0, 1   # Incrementa o índice
+        j scalar_loop      # Volta ao início do loop
+    
+    end_scalar_loop:
+    # Salva o resultado do produto escalar em memória
+    sw $t1, resultado_PE
+    
+    jr $ra  # Retorna à chamada
